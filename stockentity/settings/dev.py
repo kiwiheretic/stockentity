e1776a0f1489be224fcf9e72030397ee68596908
kiwiheretic <kiwiheretic@myself.com>
2022-12-02 12:33:57 +1300

    Initial Commit

---
+++ stockentity/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-od7gfg2_lwy=r=u9$tetb#!s-w3^^(l7c+9(s9a(^m7=!&s1m1"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
