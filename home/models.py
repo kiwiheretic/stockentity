57f020824def6c6e1f45e917eb6951b668f2292e
kiwiheretic <kiwiheretic@myself.com>
2022-12-02 14:08:58 +1300

    Add body rich text field for home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 from wagtail.models import Page
-
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 class HomePage(Page):
-    pass
+    body = RichTextField(null=True, blank=True, features=['bold','italic','link','stock'])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
